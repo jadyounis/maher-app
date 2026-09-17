1d2e986f9c43ed1ff0d81fddbd8188151d0526c2
jadyounis <ahmadyounis515@gmail.com>
2026-09-17 23:40:54 +0300

    Add professional Supabase runtime config

---
+++ professional_app/lib/app_config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  static const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const supabasePublishableKey = String.fromEnvironment('SUPABASE_PUBLISHABLE_KEY');
+
+  static bool get isConfigured =>
+      supabaseUrl.isNotEmpty && supabasePublishableKey.isNotEmpty;
+}
